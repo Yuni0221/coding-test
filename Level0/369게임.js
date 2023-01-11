35b11e507fb0aba9893211d61a78f36b365f3c9c
Yuni0221 <moonrise002002@gmail.com>
2023-01-11 14:47:51 +0900

    new update

--- Level0/369게임.js
+++ Level0/369게임.js
@@ -3,9 +3,11 @@ function solution(order) {
   var stringArray = order.toString();
   
   for (let i = 0; i < stringArray.length; i++) {
-      if (stringArray[i] % 3 == 0) {
+      if (stringArray[i] % 3 == 0 && stringArray[i] !== '0') {
           answer++;
-      }
+      } 
   }
   return answer;
 }
+
+// 0을 3으로 나누어도 나머지가 0이기 때문에 '0'을 제외하는 코드도 입력해줘야한다 
