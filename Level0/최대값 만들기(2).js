18ac8490a317062edfed55389b660c3d55f1434f
Yuni0221 <moonrise002002@gmail.com>
2023-01-11 17:56:17 +0900

    new update

---
+++ Level0/최대값 만들기(2).js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+  var newArray = numbers.sort((a, b) => b - a);
+  
+  if (newArray[newArray.length - 1] * newArray[newArray.length - 2] > newArray[0] * newArray[1]) {
+      return newArray[newArray.length - 1] * newArray[newArray.length - 2]
+  } else return newArray[0] * newArray[1]
+}
